95e70a5f47a1a6fa0b40c0276bb5b993397fddcf
andeqoo <andrew.luhring@gmail.com>
2013-10-31 20:48:10 -0500

    sets up integration script

--- jakefile.js
+++ jakefile.js
@@ -1,16 +1,33 @@
 /*global desc, task, jake, fail, complete */
+(function(){
+    "use strict";
+    desc("Test + build");
+    task("default", ["lint"]);
 
-"use strict";
+    desc("Lint all the things!");
+    task("lint", [], function(){
+        var lint = require("./build/lint/lint_runner.js")
+              , list = new jake.FileList();
+                list.include("**/*.js");
+                list.exclude("node_modules", "lib");
+        var fileList = list.toArray();
+        var options = nodeOptions();
+        lint.validateFileList(fileList, options, {});
+    });
 
-task("default", ["lint"]);
-
-desc("Lint all the things!");
-task("lint", [], function(){
-   var lint = require("./build/lint/lint_runner.js")
-       , list = new jake.FileList();
-            list.include("**/*.js");
-            list.exclude("node_modules", "lib");
-    var options = {
+    desc("Integration== make it physically impossible to have broken build.");
+    task("integrate", ["default"], function(){
+        console.log("1. Make sure 'git status' is clean");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box");
+        console.log("   b. 'get pull' ");
+        console.log("   c. 'jake'");
+        console.log("3. 'git checkout integration' ");
+        console.log("4. 'git merge master --no-ff --log' ");
+        console.log("5. 'git checkout master'");
+    });
+    function nodeOptions(){
+        return {
          bitwise: true
         , curly: true
         , eqeqeq: true
@@ -32,11 +49,10 @@ task("lint", [], function(){
         , browser: true
         , devel : true
         , jquery: true
-    };
+        };
+    }
 //
 //
 //    lint.validateFile(files.toArray(), options, {});
-var fileList = list.toArray();
-    console.log(fileList.length);
-    lint.validateFileList( fileList , options , {});
-});
+
+})();
